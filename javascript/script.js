0fc7ad4859a552e546f6f090d9a05eea9a30e51b
Maulikchhabra <46771434+Maulikchhabra@users.noreply.github.com>
2019-05-29 11:00:52 +0530

    Variables and Datatypes

--- javascript/script.js
+++ javascript/script.js
@@ -1,2 +1,11 @@
 console.log("Hello world!");
 
+//variables and datatypes//
+var firstName ="john";
+var lastName ="smith";
+var age =28;
+var job;
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(job);
