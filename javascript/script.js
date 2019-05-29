7f553455f3f3cc9bc1b6a7a9ee8884a6b88178b4
Maulikchhabra <46771434+Maulikchhabra@users.noreply.github.com>
2019-05-29 22:30:54 +0530

    Started functions

--- javascript/script.js
+++ javascript/script.js
@@ -194,3 +194,30 @@ if(height=="23"){
 23 ==="23" strictly equality (false)
 23 ===23  strictly equal (true)
 */
+
+//Functions//
+function calculateAge(birthYear) {
+  return 2018-birthYear;
+}
+var ageJohn =calculateAge(1990);
+var ageSmith =calculateAge(1995);
+var ageMax =calculateAge(2000);
+console.log(ageJohn);
+console.log(ageSmith);
+console.log(ageMax);
+
+function yearsUntilRetirement(year,firstName) {
+  var age =calculateAge(year);
+  var retirement =65-age;
+  if(retirement>0){
+    console.log(firstName+" retires in "+retirement+" years");
+  }
+  else{
+    console.log(firstName+" is already retired");
+  }
+}
+
+yearsUntilRetirement(1990,"John");
+yearsUntilRetirement(2000,"Max");
+yearsUntilRetirement(2010,"Jane");
+yearsUntilRetirement(1940,"Tony");
