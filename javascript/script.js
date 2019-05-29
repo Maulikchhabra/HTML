d68ad9bd33bee2087fbf6eddacf42655ab66c89e
Maulikchhabra <46771434+Maulikchhabra@users.noreply.github.com>
2019-05-29 11:03:54 +0530

    Operators

--- javascript/script.js
+++ javascript/script.js
@@ -34,3 +34,26 @@ alert(firstName +" "+lastName +" "+ age);
 prompt("What is his job?");
 
 */
+
+//basic operators//
+var year, yearJohn, yearMark ,ageJohn ,ageMark;
+year=2019;
+ageJohn=28;
+ageMark=30;
+yearJohn =year-ageJohn;
+yearMark =year-ageMark;
+console.log(yearJohn);
+console.log(yearMark);
+console.log(year+10);
+console.log(year*2);
+console.log(year/10);
+
+//logical operators//
+var markOlder =yearMark < yearJohn;;
+console.log(markOlder);
+
+//typeof operators//
+//higher the precedence =the first the operator works//
+console.log(typeof markOlder);
+console.log(typeof ageJohn);
+console.log(typeof firstName);
