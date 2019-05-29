670e1d8e9d5c34c86ca8e2699d0d5dad3606f9af
Maulikchhabra <46771434+Maulikchhabra@users.noreply.github.com>
2019-05-29 11:02:02 +0530

    Type coercion and variable mutation

--- javascript/script.js
+++ javascript/script.js
@@ -9,3 +9,14 @@ console.log(firstName);
 console.log(lastName);
 console.log(age);
 console.log(job);
+
+console.log(firstName +" "+lastName +" "+ age);//type coercion//
+
+var age="twenty eight";
+console.log(age); //variable mutation//
+
+job="teacher";
+console.log(job);
+
+var married=false;
+console.log(married);
