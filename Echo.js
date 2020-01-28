0a7ab476c05b4a17c5dcac46386c664e3a76968f
Maulikchhabra <46771434+Maulikchhabra@users.noreply.github.com>
2020-01-28 22:49:06 +0530

    Echo function definition

---
+++ Echo.js
@@ -0,0 +1,11 @@
+//Echo function for printing a string n times
+ function echo(str , num){
+	 var a = num;
+	 var string = str;
+	 for(var i = 0; i<a;i++){
+		 console.log(string);
+		 
+	 }
+
+ }
+ echo("hii",10);
