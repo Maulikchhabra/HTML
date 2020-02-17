a683f7c5b0b4757f876ca867c441110636e22552
Maulikchhabra <46771434+Maulikchhabra@users.noreply.github.com>
2020-02-17 18:45:00 +0530

    function nextPlayer and init done

--- Pig Game 1/app.js
+++ Pig Game 1/app.js
@@ -74,3 +74,40 @@ document.querySelector(".btn-hold").addEventListener("click",function(){
      }
 });
 
+function nextPlayer(){
+  //next player//
+  activePlayer ===0 ? activePlayer =1 :activePlayer =0; //ternary perator//
+  roundScore =0;
+  document.getElementById("current-0").textContent ="0";
+  document.getElementById("current-1").textContent ="0";
+ //document.querySelector(".player-0-panel").classList.remove("active");
+ //document.querySelector(".payer-1-panel").classList.add("active")
+  document.querySelector(".player-0-panel").classList.toggle("active");  //to transfer active from one player to other on his turn//
+  document.querySelector(".player-1-panel").classList.toggle("active");  //.................||...........//
+  document.querySelector(".dice").style.display ="none";  //to remove dice when other player gets the turn//
+}
+//this is called anonymous function where it is defined inside event listner without any name//
+
+document.querySelector(".btn-new").addEventListener("click",init);
+
+function init() { //initialize variable function//
+  scores =[0,0];
+  activePlayer =0;
+  roundScore =0;
+  gamePlaying =true;
+
+  document.querySelector(".dice").style.display ="none";
+  document.getElementById("score-0").textContent ="0";
+  document.getElementById("score-1").textContent ="0";
+  document.getElementById("current-0").textContent ="0";
+  document.getElementById("current-1").textContent ="0";
+  document.getElementById("name-0").textContent ="Player 1";
+  document.getElementById("name-1").textContent ="Player 2";
+  document.querySelector(".player-0-panel").classList.remove("winner");
+  document.querySelector(".player-1-panel").classList.remove("winner");
+  document.querySelector(".player-0-panel").classList.remove("active");
+  document.querySelector(".player-1-panel").classList.remove("active");
+  document.querySelector(".player-0-panel").classList.add("active");
+
+}
+
