e27f1cb27760f29d3d108e0c62de57e7c39079f8
Maulikchhabra <46771434+Maulikchhabra@users.noreply.github.com>
2020-02-17 18:43:38 +0530

    Two event listeners added

--- Pig Game 1/app.js
+++ Pig Game 1/app.js
@@ -24,3 +24,53 @@ document.getElementById("score-0").textContent ="0";
 document.getElementById("score-1").textContent ="0";
 document.getElementById("current-0").textContent ="0";
 document.getElementById("current-1").textContent ="0";
+
+document.querySelector(".btn-roll").addEventListener("click",function() {
+   if(gamePlaying){
+     //1. random number//
+     var dice =Math.floor(Math.random()*6) +1;
+
+     //2.display the result
+     var diceDOM =document.querySelector(".dice");
+     diceDOM.style.display ="block";
+     diceDOM.src ="images/dice-"+dice+".png"
+
+
+     //3.update the round score if rolled number was not a 1.
+     if(dice!==1){
+       //add score
+       roundScore +=dice;
+       document.querySelector("#current-" + activePlayer).textContent=roundScore;
+       //roundScore =roundScore+ dice;
+     }
+     else{
+       //next player//
+      nextPlayer();
+     }
+   }
+});
+
+document.querySelector(".btn-hold").addEventListener("click",function(){
+     if(gamePlaying){
+       //Add current score to global score
+       scores[activePlayer] += roundScore;
+      //scores[activePlayer] =scores[active] +roundScore;
+
+      //Update the UI
+      document.querySelector("#score-"+ activePlayer).textContent =scores[activePlayer];
+
+      //Check if player won the game
+      if(scores[activePlayer] >= 20){
+        document.querySelector("#name-"+activePlayer).textContent ="Winner!";
+        document.querySelector(".dice").style.display ="none";
+        document.querySelector(".player-"+activePlayer +"-panel").classList.add("winner");
+        document.querySelector(".player-"+activePlayer +"-panel").classList.remove("winner");
+        gamePlaying =false;
+      }
+      else{
+        //next player//
+        nextPlayer();
+      }
+     }
+});
+
