ea0b6243aba63b1bf5f69f8aff4532bcf8d8989a
Maulikchhabra <46771434+Maulikchhabra@users.noreply.github.com>
2020-03-04 18:00:28 +0530

    Create app.js

---
+++ Pig Game 2/app.js
@@ -0,0 +1,14 @@
+var scores,roundScore,activePlayer,dice,gamePlaying;
+var lastDice;
+
+init();
+//dice =Math.floor(Math.random()*6) +1;
+//console.log(dice);
+
+//document.querySelector("#current-" + activePlayer).textContent =dice; //setter//
+//document.querySelector("#current-"+activePlayer).innerHTML ="<em>"+ dice+ "</em>";
+
+//var x = document.querySelector("#score-0").textContent; //getter//
+//console.log(x);
+
+document.querySelector(".dice").style.display ="none";
